89e029f8dd6cd905d5ab88dc38696e2afb6bf333
Veronazavr <verochka10@list.ru>
2020-10-25 18:25:30 +0300

    added changed

--- src/main/java/ru/netology/domain/Post.java
+++ src/main/java/ru/netology/domain/Post.java
@@ -1,4 +1,268 @@
 package ru.netology.domain;
 
 public class Post {
+    private int id;
+    private int userId;
+    private int ownerId;
+    private int createdBy;
+    private int data;
+    private String textPost;
+    private int replyOwnerId;
+    private int replyPostId;
+    private int friendsOnly;
+
+    public String comments(int count, boolean canPost, boolean groupsCanPost, boolean canOpen) {
+        return null;
+    }
+
+    private String copyright;
+
+    public String likes(int count, boolean userLikes, boolean canLike, boolean canPublish) {
+        return null;
+    }
+
+    public String reposts (int count, boolean userReposted) {
+        return null;
+    }
+
+    public String views (int count) {
+        return null;
+    }
+
+    private String postType;
+
+    public String geo (String type, String coordinates, String place) {
+        return null;
+    }
+
+    private int signerId;
+    private int canPin;
+    private int canDelete;
+    private int canEdit;
+
+    private int isPinned;
+    private int markedAsAds;
+    private boolean isFavorite;
+    private int postponedId;
+
+    private String subjectId;
+    private String urlSource;
+    private boolean signatureView;
+
+    // Social networks
+    private int likeSum;
+    private int commentSum;
+    private int shareSum;
+    private int viewSum;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(int ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public int getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(int createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public String getTextPost() {
+        return textPost;
+    }
+
+    public void setTextPost(String textPost) {
+        this.textPost = textPost;
+    }
+
+    public int getReplyOwnerId() {
+        return replyOwnerId;
+    }
+
+    public void setReplyOwnerId(int replyOwnerId) {
+        this.replyOwnerId = replyOwnerId;
+    }
+
+    public int getReplyPostId() {
+        return replyPostId;
+    }
+
+    public void setReplyPostId(int replyPostId) {
+        this.replyPostId = replyPostId;
+    }
+
+    public int getFriendsOnly() {
+        return friendsOnly;
+    }
+
+    public void setFriendsOnly(int friendsOnly) {
+        this.friendsOnly = friendsOnly;
+    }
+
+    public String getCopyright() {
+        return copyright;
+    }
+
+    public void setCopyright(String copyright) {
+        this.copyright = copyright;
+    }
+
+    public String getPostType() {
+        return postType;
+    }
+
+    public void setPostType(String postType) {
+        this.postType = postType;
+    }
+
+    public int getSignerId() {
+        return signerId;
+    }
+
+    public void setSignerId(int signerId) {
+        this.signerId = signerId;
+    }
+
+    public int getCanPin() {
+        return canPin;
+    }
+
+    public void setCanPin(int canPin) {
+        this.canPin = canPin;
+    }
+
+    public int getCanDelete() {
+        return canDelete;
+    }
+
+    public void setCanDelete(int canDelete) {
+        this.canDelete = canDelete;
+    }
+
+    public int getCanEdit() {
+        return canEdit;
+    }
+
+    public void setCanEdit(int canEdit) {
+        this.canEdit = canEdit;
+    }
+
+    public int getIsPinned() {
+        return isPinned;
+    }
+
+    public void setIsPinned(int isPinned) {
+        this.isPinned = isPinned;
+    }
+
+    public int getMarkedAsAds() {
+        return markedAsAds;
+    }
+
+    public void setMarkedAsAds(int markedAsAds) {
+        this.markedAsAds = markedAsAds;
+    }
+
+    public boolean isFavorite() {
+        return isFavorite;
+    }
+
+    public void setFavorite(boolean favorite) {
+        isFavorite = favorite;
+    }
+
+    public int getPostponedId() {
+        return postponedId;
+    }
+
+    public void setPostponedId(int postponedId) {
+        this.postponedId = postponedId;
+    }
+
+    public String getSubjectId() {
+        return subjectId;
+    }
+
+    public void setSubjectId(String subjectId) {
+        this.subjectId = subjectId;
+    }
+
+    public String getUrlSource() {
+        return urlSource;
+    }
+
+    public void setUrlSource(String urlSource) {
+        this.urlSource = urlSource;
+    }
+
+    public boolean isSignatureView() {
+        return signatureView;
+    }
+
+    public void setSignatureView(boolean signatureView) {
+        this.signatureView = signatureView;
+    }
+
+    public int getLikeSum() {
+        return likeSum;
+    }
+
+    public void setLikeSum(int likeSum) {
+        this.likeSum = likeSum;
+    }
+
+    public int getCommentSum() {
+        return commentSum;
+    }
+
+    public void setCommentSum(int commentSum) {
+        this.commentSum = commentSum;
+    }
+
+    public int getShareSum() {
+        return shareSum;
+    }
+
+    public void setShareSum(int shareSum) {
+        this.shareSum = shareSum;
+    }
+
+    public int getViewSum() {
+        return viewSum;
+    }
+
+    public void setViewSum(int viewSum) {
+        this.viewSum = viewSum;
+    }
 }
+
+

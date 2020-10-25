f88b52b957b3ae6881d2c1c4d83ce2639ab65fd0
Veronazavr <verochka10@list.ru>
2020-10-25 19:37:48 +0300

    added updated Manager

---
+++ src/main/java/ru/netology/domain/Manager/PostManager.java
@@ -0,0 +1,17 @@
+package ru.netology.domain.Manager;
+
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] search(int ownerID, String domain, String query, boolean ownersOnly, byte count, int offset) {
+        return null;
+    }
+
+    public void delete(int ownerID, int postID) {
+    }
+
+}
+
